5d2fcd7f18b101db9920de1f49390ead846e79f4
GenaBitu <genabitu@gmail.com>
2018-12-20 17:10:07 +0100

    Removed config roundtrip

--- src/js/config.js
+++ src/js/config.js
@@ -1,23 +1,10 @@
 "use strict";
-/* global CONFIG:true */
-/* exported CONFIG, configSetup */
+/* exported configSetup */
 
-var CONFIG;
 var configEvent = new AfterLoadEvent(1);
 
 function configSetup()
 {
-	var configRequest = new XMLHttpRequest();
-	configRequest.overrideMimeType("application/json");
-	configRequest.open('GET', '/client-config.json', true);
-	configRequest.onreadystatechange = function ()
-		{
-			if(this.readyState === 4 && this.status === 200)
-			{
-				CONFIG = JSON.parse(this.responseText);
-				CONFIG.cache = "odymaterialy-" + ""/*INJECTED-VERSION*/;
-				configEvent.trigger();
-			}
-		};
-	configRequest.send();
+	CONFIG.cache = "odymaterialy-" + ""/*INJECTED-VERSION*/;
+	configEvent.trigger();
 }
