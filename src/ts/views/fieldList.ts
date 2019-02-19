8c2ab88ba4de3839694c93a1c40855ac216125e2
GenaBitu <genabitu@gmail.com>
2019-02-20 00:53:49 +0100

    Changed card design

--- src/ts/views/fieldList.ts
+++ src/ts/views/fieldList.ts
@@ -3,24 +3,18 @@
 
 function renderFullFieldList(): string
 {
-	var html = "";
+	var html = "<div id=\"field-list-col1\" class=\"field-list-col\">";
 	for(var i = 0; i < FULLFIELDS.length; i++)
 	{
 		html += "<div class=\"field-card\" data-id=\"" + FULLFIELDS[i].id + "\">"
 		html += "<img class=\"field-card-image\" src=\"" + CONFIG.apiuri + "/image/" + FULLFIELDS[i].image + "\">";
-		html += "<svg width=\"" + CONFIG['custom-properties']['--field-card-width'] + "\" height=\"" + CONFIG['custom-properties']['--field-card-height'] + "\" viewBox=\"0 " + (parseInt(CONFIG['custom-properties']['--field-card-height']) - parseInt(CONFIG['custom-properties']['--field-card-overlay-height'])) + " " + CONFIG['custom-properties']['--field-card-width'] + " " + CONFIG['custom-properties']['--field-card-height'] + "\" class=\"field-card-svg\">";
-		html += "<defs>";
-		html += "<filter id=\"blur\">";
-		html += "<feGaussianBlur in=\"SourceGraphic\" stdDeviation=\"3\" />";
-		html += "</filter>";
-		html += "</defs>";
-		html += "<image filter=\"url(#blur)\" xlink:href=\"" + CONFIG.apiuri + "/image/" + FULLFIELDS[i].image + "\" x=\"0\" y=\"0\" height=\"" + CONFIG['custom-properties']['--field-card-height'] + "px\" width=\"" + CONFIG['custom-properties']['--field-card-width'] + "px\" preserveAspectRatio=\"none\" />";
-		html += "</svg>";
-		html += "<div class=\"field-card-overlay\">";
+		html += "<h2 class=\"field-card-heading\">";
 		html += FULLFIELDS[i].name;
-		html += "</div>";
-		html += "</div>";
+		html += "</h2><div class=\"field-card-description\">";
+		html += "Lorem ipsum dolor sit amet consectetur adipiscing elit.";
+		html += "</div></div>";
 	}
+	html += "</div><div id=\"field-list-col2\" class=\"field-list-col\"></div>";
 	return html;
 }
 
@@ -40,11 +34,15 @@ function renderFieldListView(noHistory: boolean): void
 	var html = "<h1>" + CONFIG["site-name"] + "</h1>";
 	html += renderFullFieldList();
 	document.getElementById("content")!.innerHTML = html;
-
 	var nodes = document.getElementById("content")!.getElementsByClassName("field-card");
-	for(var l = 0; l < nodes.length; l++)
+	var height = nodes.length / 2;
+	for(var l = nodes.length - 1; l >= 0; l--)
 	{
 		(nodes[l] as HTMLElement).onclick = fieldListOnClick;
+		if(l > height) {
+			var col = document.getElementById("field-list-col2")!;
+			col.insertBefore(nodes[l], col.firstChild);
+		}
 	}
 	document.getElementsByTagName("main")[0].scrollTop = 0;
 	if(!noHistory)
