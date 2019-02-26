3fe471f60838f803f545c35d9b5c6a637b6c49e1
GenaBitu <genabitu@gmail.com>
2019-02-26 14:38:38 +0100

    Removed old fields from CustomProperties interface

--- src/ts/interfaces/CustomProperties.ts
+++ src/ts/interfaces/CustomProperties.ts
@@ -1,7 +1,5 @@
 interface CustomProperties {
 	"--accent-color": string;
-	"--field-card-height": string;
-	"--field-card-overlay-height": string;
 	"--field-card-width": string;
 	"--nav-width": string;
 }
