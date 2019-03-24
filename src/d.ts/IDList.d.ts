b4401f0dd525a584b6fb535bc5f4d44eaf4e7834
GenaBitu <genabitu@gmail.com>
2019-03-24 16:42:00 +0100

    Added IDList.filter

--- src/d.ts/IDList.d.ts
+++ src/d.ts/IDList.d.ts
@@ -1,6 +1,7 @@
 declare class IDList<T>
 {
 	public iterate(iterator: (key: string, value: T) => void): void;
+	public filter(filter: (key: string, value: T) => boolean): IDList<T>;
 	public get(key: string): T;
 	public push(key: string, value: T): void;
 }
