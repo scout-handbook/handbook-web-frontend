bd1e2fed524ed19243a1584223ba5319b898c76c
Marek Dědič <developer@dedic.eu>
2021-04-20 20:02:23 +0200

    Lint fix

--- src/d.ts/IDList.d.ts
+++ src/d.ts/IDList.d.ts
@@ -2,6 +2,7 @@ declare class IDList<T>
 {
 	public iterate(iterator: (key: string, value: T) => void): void;
 	public filter(filter: (key: string, value: T) => boolean): IDList<T>;
+	public empty(): boolean;
 	public get(key: string): T;
 	public push(key: string, value: T): void;
 }
