efbce75596469fd2b0d8d9bd4b2f680866e6cd1f
Marek Dědič <developer@dedic.eu>
2022-09-03 11:13:07 +0200

    CLeaned up d.ts files

--- src/d.ts/AfterLoadEvent.d.ts
+++
@@ -1,5 +0,0 @@
-declare class AfterLoadEvent {
-  public addCallback(callback: (...args: Array<RequestResponse>) => void): void;
-  public trigger(...args: Array<RequestResponse>): void;
-  public retrigger(...args: Array<RequestResponse>): void;
-}
